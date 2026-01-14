67cb50181f6376efde92249f14e6fdcf195f70d7
Leon Hurtado <leon@zype.com>
2026-01-14 18:46:30 -0300

    lottie text editor!

--- index.js
+++ index.js
@@ -174,7 +174,7 @@ anim.addEventListener('config_ready', function (e) {
     })
 
     //Add fonts to style
-    if (!fontsLoaded) {
+    if (!fontsLoaded && anim.renderer.data.fonts) {
         let fonts = anim.renderer.data.fonts.list;
         for (const font in fonts) {
             let family = fonts[font].fFamily
